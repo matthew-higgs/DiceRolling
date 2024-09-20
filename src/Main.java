af0af58323096a52f57f935e28c1256f6182841a
Chewbacca <higgimat000@warren.k12.in.us>
2024-09-20 13:29:31 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class main {
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+    }
+
+    public static void rollSixDice() {
+        Random rand = new Random();
+        System.out.print("6 dices roll: ");
+        for (int i = 0; i < 6; i++) {
+            int roll = rand.nextInt(6) + 1;
+            System.out.print("Roll #" + (i + 1) + ": " + roll + " ");
+        }
+        System.out.println();
+    }
 }
